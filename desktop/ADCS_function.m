b222b5fc28c97ca5a7f24a93efb4b07bff0afaef
Karl Olfe <kolfe@eusoc.upm.es>
2022-10-06 20:13:05 +0200

    Add I/O descriptions in the function Correct app window title Set the initial cam PoV to LVLH frame

--- desktop/ADCS_function.m
+++ desktop/ADCS_function.m
@@ -1,32 +1,24 @@
 function [TcmdWhl, m, auxiliar] = ADCS_function(current_time, svb, bvb, wn, qbn, HWhl, PosN, VelN, auxiliar)
-% Finds the rotation matrix and the quaternion given 2 vectors readings 
-%   in body coordinates using the TRIAD method.
+% Attitude control algorithm. This function receives the sensors readings of a simulated spacecraft and command its actuators.
 
-%
-% Inputs            Description                                         Units
-%   vectors_body    Array[3x2] : 2 vectors in Body coordinates.         [N/A]
-%       vectors_body(:,1) : sun vector
-%       vectors_body(:,2) : magnetometer vector                         Units
-%   vectors_inertial    Array[3x2] : 2 vectors in inertial coordinates. [N/A]
-%       vectors_inertial(:,1) : sun vector
-%       vectors_inertial(:,2) : magnetometer vector
+% Inputs            Description [Units]
+%   current_time    datetime object: simulated time [N/A]
+%   svb             Array[1x3]: Sun-pointing unit Vector, expressed in Body frame [N/A]
+%   bvb             Array[3x1]: magnetic field in Body frame [Tesla]
+%   wn              Array[3x1]: Angular Velocity of Body frame wrt iNertial frame expressed in Body frame [rad/sec]
+%   qbn             Array[4x1]: attitude Quaternion of Body frame in iNertial frame (scalar part is the first element) [N/A]
+%   HWhl            Array[1x3]: Wheels Angular Momentum [Nms]
+%   PosN            Array[3x1]: Position expressed in iNertial frame [m]
+%   VelN            Array[3x1]: Velocity expressed in iNertial frame [m/sec]
+   
 % Outputs
-%   R               Array[3x3] : attitude rotation matrix               [N/A]
-%   Q               Array[4x1] : attitude quaternion                    [N/A]
+%   TcmdWhl         Array[1x3]: Torque commanded to the wheels [Nm]
+%   m               Array[1x3] : Magnetic dipole commanded to the magnetorquers [Am^2]
 
-%% To add noise uncomment the following lines:
-%     % sun vector with gaussian noise:
-%     vectors_body(:,1) = vectors_body(:,1) + randn(3,1)/50;
-%     % sun vector with bias noise:
-%     vectors_body(:,1) = vectors_body(:,1) + 0.1*ones(3,1);
+% auxiliar is a variable available to get previous value of any other defined varible. 
+% It is intended to be used for the derivative and integral parts of a PID controller.
+% It is initially defined as a all zeros array[1x10]
 
-%     % magnetic vector with gaussian noise:
-%     vectors_body(:,2) = vectors_body(:,2) + randn(3,1)/50;
-%     % magnetic vector with bias noise:
-%     vectors_body(:,2) = vectors_body(:,2) + 0.1*ones(3,1);
-
-
-%% TRIAD algorithm
 m=[0 0 0];
 TcmdWhl = [0 0 0];
 end
