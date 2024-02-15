92d18141ad8291cd62f71d569b64a251d18660e1
Karl Olfe <kolfe@eusoc.upm.es>
2024-02-15 16:46:10 +0100

    Changes to execute from user directory instead from root /opt

--- desktop/startADCS.sh
+++ desktop/startADCS.sh
@@ -1,2 +1,2 @@
 
-matlab -nodesktop -r "addpath '/opt/42/MUIA_DCCVE_AttitudeControl/desktop'; ADCS_program"
+matlab -nodesktop -r "addpath '/home/upm/42/MUIA_DCCVE_AttitudeControl/desktop'; ADCS_program"
